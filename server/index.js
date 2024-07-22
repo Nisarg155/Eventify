0f78786d9b5791d3e0574ebf6dd23308cd9d2d34
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-07-23 00:22:05 +0530

    server config

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+
+dotenv.config();
+const app = express();
+
+
+mongoose.connect(process.env.DATABASE_URL).then(
+    () => console.log('MongoDB Connected'),
+)
+    .catch(err => console.log(err))
+
+
+
+
+
+
