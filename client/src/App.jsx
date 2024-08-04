638e50adc8d1227b23e471f8f6938883d841ae39
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-08-04 12:45:38 +0530

    frontend firebase config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -29,6 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+
   )
 }
 
---
+++ client/src/firebaseconfig.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: import.meta.env.REACT_APP_API_KEY,
+    authDomain: import.meta.env.REACT_APP_DOMAIN,
+    projectId: import.meta.env.REACT_APP_PROJECTID,
+    storageBucket: import.meta.env.REACT_APP_BUCKET,
+    messagingSenderId: import.meta.env.REACT_APP_MESSAGESENDERID,
+    appId: import.meta.env.REACT_APP_APPID,
+    measurementId: import.meta.env.REACT_APP_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
