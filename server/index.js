f0a8be8ccc2a6ef475e600c97fcee3a1e6ac51f5
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-08-12 22:07:29 +0530

    mailgun config added

--- server/index.js
+++ server/index.js
@@ -5,9 +5,24 @@ const cors = require('cors');
 const morgan = require('morgan');
 const {protect} = require("./auth/auth");
 const loginRouter = require('./router/login');
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+dotenv.config();
+const mailgun = require("mailgun-js");
+const DOMAIN = "mailg.csi-ddu.tech";
+
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
+const data = {
+    from: "CSI DDU <ce_csi@ddu.ac.in>",
+    to: "amlaninisarg15@gmail.com",
+    subject: "Hello",
+    text: "Testing some Mailgun awesomness!"
+};
+mg.messages().send(data, function (error, body) {
+    console.log(body);
+});
+
+// const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
 
 mongoose.connect(process.env.DATABASE_URL).then(
     (connection) => {
